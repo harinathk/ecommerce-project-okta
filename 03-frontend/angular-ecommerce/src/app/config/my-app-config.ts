501ba86e8d1c7279aef1ca078a66888116356a2f
Harinath <harinathk7@gmail.com>
2022-04-16 18:26:10 +0800

    Update with okta changes

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc:{
+        clientId:'0oa4md7ipwjCtu9gB5d7',
+        issuer:'https://dev-38133729.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes:['openid','profile','email']
+    }
+}
